5bfe4c22b09faf124ad4f55528f0ef373a6ae5c4
ANKIT03RANA <ankitranaji3@gmail.com>
2023-09-28 20:48:56 +0530

    Form created to add posts

--- First_Project/ui/src/components/Posts/Posts.js
+++ First_Project/ui/src/components/Posts/Posts.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import Post from './Post/Post';
 import useStyles from "./styles.js";
+import { useSelector } from 'react-redux';
+import { CircularProgress, Grid } from '@material-ui/core';
 
 const Posts=()=>{
     const classes = useStyles();
+    const posts = useSelector((state)=>state.posts);
     return (
-        <>
-        <h1>Posts</h1>
-        <Post/>
-        <Post/>
-        <Post/>
-        </>
+        !posts.length ? <CircularProgress/> : (
+            <Grid className={classes.container} container alignItems='stretch' spacing={3}>
+                {posts.map((post)=>(
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     );
 }
 
