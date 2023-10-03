909c638107b5ee922c82076767ed3b9648e8bf63
ANKIT03RANA <ankitranaji3@gmail.com>
2023-10-03 19:50:42 +0530

    implementing authentication

--- First_Project/ui/src/components/Auth/Auth.js
+++ First_Project/ui/src/components/Auth/Auth.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Typography, Paper, Grid, Avatar, Button, Container } from "@material-ui/core";
 import useStyles from "./styles.js"
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined.js"
 
 function Auth() {
     const classes = useStyles();
+    const isSignUp=false;
   return (
-    <div>
-      AUTH
-    </div>
+    <Container component="main" maxWidth="xs" >
+        <Paper className={classes.paper} elevation={3} >
+            <Avatar className={classes.avatar}>
+                <LockOutlinedIcon/>
+            </Avatar>
+            <Typography variant='h5'>{isSignUp?'Sign Up':'Sign In'}</Typography>
+            <form className={classes.form} onSubmit={handleSubmit}></form>
+        </Paper>
+    </Container>
   )
 }
 
