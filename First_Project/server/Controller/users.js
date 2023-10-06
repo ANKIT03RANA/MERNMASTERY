cf084eee04e6477af7203a1afb1420b109e27174
ANKIT03RANA <ankitranaji3@gmail.com>
2023-10-06 23:18:46 +0530

    user needs to be created

---
+++ First_Project/server/Controller/users.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'
+
+import User from "../models/user";
+
+export const signin = async(req,res)=>{
+    
+}
+
+export const signup = async(req,res)=>{
+
+}
