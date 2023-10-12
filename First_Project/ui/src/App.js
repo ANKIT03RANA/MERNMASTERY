2e8ff3f8f3a117c1384ec341c4d947980e471e86
ANKIT03RANA <ankitranaji3@gmail.com>
2023-10-12 21:08:13 +0530

    pagination frontend logic implemented and routing improved

--- First_Project/ui/src/App.js
+++ First_Project/ui/src/App.js
@@ -4,20 +4,19 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
-  
   return (
     <BrowserRouter>
-    <Container maxWidth="lg">
-      <Navbar/>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/auth" exact component={Auth} />
-      </Switch>
-      
-    </Container>
-    
+      <Container maxWidth="lg">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={() => <Redirect to="/posts" />} />
+          <Route path="/posts" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
+        </Switch>
+      </Container>
     </BrowserRouter>
   );
 }
