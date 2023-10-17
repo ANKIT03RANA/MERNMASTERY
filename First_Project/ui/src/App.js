5b83725d148c2e4da951ebdeed854e788ec9a20d
ANKIT03RANA <ankitranaji3@gmail.com>
2023-10-17 20:18:26 +0530

    adding pagination

--- First_Project/ui/src/App.js
+++ First_Project/ui/src/App.js
@@ -7,6 +7,7 @@ import Auth from "./components/Auth/Auth";
 import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
+  const user = JSON.parse(localStorage.getItem("profle"));
   return (
     <BrowserRouter>
       <Container maxWidth="lg">
@@ -14,7 +15,11 @@ function App() {
         <Switch>
           <Route path="/" exact component={() => <Redirect to="/posts" />} />
           <Route path="/posts" exact component={Home} />
-          <Route path="/auth" exact component={Auth} />
+          <Route
+            path="/auth"
+            exact
+            component={!user ? <Auth /> : <Redirect to="/posts" />}
+          />
         </Switch>
       </Container>
     </BrowserRouter>
--- First_Project/ui/src/components/Home/Home.js
+++ First_Project/ui/src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Grid, Grow } from "@material-ui/core";
+import { Container, Grid, Grow, Paper } from "@material-ui/core";
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 import { useDispatch } from 'react-redux';
 import { getPosts } from '../../actions/posts';
 import useStyles from "../../styles.js";
+import Paginate from '../Pagination/Pagination';
 
 function Home() {
     const [currentID, setCurrentID] = useState(null);
@@ -28,6 +29,9 @@ function Home() {
             <Grid item xs={12} sm={4}>
               <Form currentID={currentID} setCurrentID={setCurrentID} />
             </Grid>
+            <Paper className={classes.pagination} elevation={6}>
+              <Paginate/>
+            </Paper>
           </Grid>
         </Container>
       </Grow>
